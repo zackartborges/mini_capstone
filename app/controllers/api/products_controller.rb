c2a196fec5c954a86e71aba661d497caf4d17982
Zack Borges <zackartborges@gmail.com>
2021-03-04 15:29:07 -0500

    tried to edit spacing error

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,23 +4,11 @@ class Api::ProductsController < ApplicationController
     render "index.json.jb"
   end
 
-  # def all_individual_products
-  #   @products = Product.all
-  #   individual_products = @products.each do |product|
-  #   end
-  #   @products = individual_products
-  #   render "product.json.jb"
-  # end
-
   def show
     # item = params[:item_select]
     @product = Product.find(params[:id])
     render "show.json.jb"
   end
-
-  # def username_password
-  #   @products
-  # end
 
   def create
     @product = Product.new(
@@ -39,6 +27,7 @@ class Api::ProductsController < ApplicationController
   def update
     id = params[:id]
     @product = Product.find(id)
+
     (@product.name = params[:name] || @product.name
       @product.price = params[:price] || @product.price
       @product.image_url = params[:image_url] || @product.image_url
