8858679733b8aa5e38024afbae6cb13649158839
Zack Borges <zackartborges@gmail.com>
2021-03-08 08:47:29 -0500

    fixes validation errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :inventory, presence: true, numericality: { greater_than: 0 }
+  validates :inventory, presence: true
+  validates :inventory, numericality: { greater_than: 0 }
   validates :price, numericality: { greater_than: 0 }
   validates :name, uniqueness: true
-  validates :description, length: { in: 20..200 }
-  validates :description, length: { minimum: 20 }
+  validates :description, length: { in: 5..200 }
 
   def is_discounted?
     price <= 10
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 # Product.create(name: "Poker Chips set", price: 20.00, image_url: "hd836", description: "Great poker chips!")
 
 # Product.create(name: "Scorpion King in blu ray", price: 350.00, image_url: "sjwuahe", description: "Best movie ever!")
-Supplier.create(name: "Big City Classic Time Items!", email: "wowwowitems@example.com", phone_number: 39200139)
+# Supplier.create(name: "Big City Classic Time Items!", email: "wowwowitems@example.com", phone_number: 39200139)
